f08a593a7545a75fc7d5ceeecfed024e56be4233
Lamor Odingo <l2odingo@gmail.com>
2022-03-26 00:07:17 -0700

    created defined model columns

--- models/Category.js
+++ models/Category.js
@@ -7,6 +7,23 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
