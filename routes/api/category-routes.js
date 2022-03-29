b5dbedeebb966e95bd1f132ffcb1924574f7e8f5
Lamor Odingo <l2odingo@gmail.com>
2022-03-28 18:10:37 -0700

    created put request for category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,8 +6,10 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  try{
-  const categoryData = await Category.findAll();
+  try {
+  const categoryData = await Category.findAll({
+    include: [{model: Product}]
+  });
   res.status(200).json(categoryData);
 }
 catch (err) {
@@ -18,25 +20,82 @@ catch (err) {
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-  // try {
-  //   const categoryData = await Category/
-  // }
-  // catch {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}] 
+    })
 
-  // }
+    if(categoryData) {
+      res.status(200).json(categoryData);
+    }
+    else {
+      res.status(404).json({message: "No category is found with that id!"})
+    }
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body) 
+
+    if(newCategory) {
+      res.status(200).json(newCategory);
+    }
+    else {
+      req.status(404).json({message: "The request for a new category_name failed. Request must contain category_name"});
+    }
+
+
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    if(req.body && req.body.category_name) {
+      const updateCategory = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+      )
+      
+    if(!updateCategory[0]) {
+      res.status(404).json({message: "There is no category with this id"})
+    }
+    else {
+      res.status(200).json(updateCategory)
+    }
+  }
+    else {
+      res.status(404).json({message: "Update request failed. Request must contain category_name"})
+    }
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  }
+  catch (err) {
+    
+  }
 });
 
 module.exports = router;
