4ff0d7b1ce3d141b5a16cc8b3dda48969493e425
Lamor Odingo <l2odingo@gmail.com>
2022-04-03 00:00:33 -0700

    created product-route info

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,18 +1,46 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+try {
+  const products = await Product.findAll({
+    include: [{model: Category},{model: Tag, through: ProductTag}
+    ]
+  })
+  res.status(200).json(products);
+}
+catch (err) {
+  console.log(`Error in finding all available products: ${err}`);
+  res.status(500).json(err);
+}
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProduct = await Product.findByPk(req.params.id, {
+      include: [{model: Category},{model: Tag, through: ProductTag}]
+    });
+
+    if (singleProduct) {
+      res.status(200).json(singleProduct);
+    }
+    else {
+      res.status(404).json({message: "There is no product with that id!"});
+    }
+  }
+  catch (err) {
+    console.log(`Error in finding product by its unique id: ${err}`);
+    res.status(500).json(err);
+  } 
 });
 
 // create new product
@@ -89,8 +117,41 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    // This approach is called to delete all references to the product from the product_tag table
+    const deleteProductTag = await ProductTag.destroy({
+      where: {
+        product_id: req.params.id
+      }
+    });
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (deleteProductTag) {
+      res.status(200).json(deleteProductTag);
+    }
+
+    else {
+      res.status(404).json({message: "There is no product with that id!"});
+    }
+
+    if (deleteProduct) {
+      res.status(200).json(deleteProduct);
+    }
+    
+    else {
+      res.status(404).json({message: "There is not a ProductTag with that id!"});
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
